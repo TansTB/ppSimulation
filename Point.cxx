e34da729e2549521a442f641490a41bb7d0eb0c4
AndreaBellora <andrea.bellora@gmail.com>
2016-11-16 11:00:13 +0100

    Update Point.cxx

--- Point.cxx
+++ Point.cxx
@@ -6,6 +6,8 @@ Point::Point(const char *name, Double_t x, Double_t y, Double_t z){
   this->z = z;
   this->SetName(name);
 }
+
 void Point::SetX(Double_t x){this->x = x;}
 void Point::SetY(Double_t y){this->y = y;}
 void Point::SetZ(Double_t z){this->z = z;}
+void Point::Print(){std::cout<<"x: " << x << "\ny: " << y << "\nz: " << z << std::endl;}
