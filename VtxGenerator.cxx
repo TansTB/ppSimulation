788ce4948656e3fa93113b73f62803da2b10aa97
AndreaBellora <andrea.bellora@gmail.com>
2016-11-17 16:54:26 +0100

    MultiplicityGenerator COMPLETED
    Added GetConst methods for possible uses in the run manager
    Added copyconstructors and assignment operators as private members as they shouldn't be used

--- VtxGenerator.cxx
+++ VtxGenerator.cxx
@@ -4,34 +4,56 @@ VtxGenerator::VtxGenerator(const char *input_file_name, const char* input_hist_n
   this->input_file_name = input_file_name;
   this->input_hist_name=input_hist_name;
   this->input_file = new TFile(input_file_name);
-  this->input_hist = (TH1D*) input_file->Get(input_hist_name);
-  this->input_hist->SetDirectory(0);
+  this->xinput_hist = (TH1D*) input_file->Get(xinput_hist_name);
+  this->yinput_hist = (TH1D*) input_file->Get(yinput_hist_name);
+  this->zinput_hist = (TH1D*) input_file->Get(zinput_hist_name);
+  this->xinput_hist->SetDirectory(0);
+  this->yinput_hist->SetDirectory(0);
+  this->zinput_hist->SetDirectory(0);
   this->input_file->Close();
   delete input_file;
   this->used_hist = kTRUE;
 }
 
 VtxGenerator::~VtxGenerator(){
-  if(used_hist)delete input_hist;
+  if(used_hist){
+    delete xinput_hist;
+    delete yinput_hist;
+    delete zinput_hist;
+  }
 }
 
 void VtxGenerator::SetCustomInput(const char* input_file_name, const char* input_hist_name){
   this->input_file_name = input_file_name;
   this->input_hist_name=input_hist_name;   
   this->input_file = new TFile(input_file_name);
-  this->input_hist = (TH1D*) input_file->Get(input_hist_name);
-  this->input_hist->SetDirectory(0);
+  this->xinput_hist = (TH1D*) input_file->Get(xinput_hist_name);
+  this->yinput_hist = (TH1D*) input_file->Get(yinput_hist_name);
+  this->zinput_hist = (TH1D*) input_file->Get(zinput_hist_name);
+  this->xinput_hist->SetDirectory(0);
+  this->yinput_hist->SetDirectory(0);
+  this->zinput_hist->SetDirectory(0);
   this->input_file->Close();
   delete input_file;
   this->used_hist = kTRUE;
 }
 
-Point* GetGausVtx(Double_t x, Double_t sx, Double_t y, Double_t sy, Double_t z, Double_t sz){
+Point* VtxGenerator::GetConstVtx(Double_t x, Double_t y, Double_t z){
+  Point *o_point = new Point(x,y,z);
+  return o_point;  
+}
+
+Point* VtxGenerator::GetGausVtx(Double_t x, Double_t sx, Double_t y, Double_t sy, Double_t z, Double_t sz){
   Point *o_point = new Point(gRandom->Gaus(x,sx),gRandom->Gaus(y,sy),gRandom->Gaus(z,sz));
   return o_point;
 }
 
-Point* GetUniformVtx(Double_t xmin, Double_t xmax, Double_t ymin, Double_t ymax, Double_t zmin, Double_t zmax){
+Point* VtxGenerator::GetUniformVtx(Double_t xmin, Double_t xmax, Double_t ymin, Double_t ymax, Double_t zmin, Double_t zmax){
   Point *o_point = new Point(gRandom->Uniform(xmin,xmax),gRandom->Uniform(ymin,ymax),gRandom->Uniform(zmin,zmax));
+  return o_point;
+}
+
+Point* VtxGenerator::GetCustomVtx(){
+  Point *o_point = new Point(xinput_hist->GetRandom(),yinput_hist->GetRandom(),zinput_hist->GetRandom(),);
   return o_point;
 }
--- VtxGenerator.h
+++ VtxGenerator.h
@@ -2,28 +2,40 @@
 #define VTXGENERATOR_H
 #if !defined(__CINT__) || defined(__MAKECINT__)
 #include "Point.h"
-#include "TRandom3.h"
 #include "TFile.h"
+#include "TH1D.h"
 #endif
 
 class VtxGenerator{
   public:
     VtxGenerator(){}
-    VtxGenerator(const char *input_file_name, const char* input_hist_name);
+    VtxGenerator(const char* xinput_file_name, const char* xinput_hist_name,const char* yinput_file_name, const char* yinput_hist_name,const char* zinput_file_name, const char* zinput_hist_name);
     ~VtxGenerator();
         
-    void SetCustomInput(const char* input_file_name, const char* input_hist_name);
+    void SetCustomInput(const char* xinput_file_name, const char* xinput_hist_name,const char* yinput_file_name, const char* yinput_hist_name,const char* zinput_file_name, const char* zinput_hist_name);
     const char* GetInputFileName(){return input_file_name;}
-    const char* GetInputHistName(){return input_hist_name;}
+    const char* GetXInputHistName(){return xinput_hist_name;}
+    const char* GetYInputHistName(){return yinput_hist_name;}
+    const char* GetZInputHistName(){return zinput_hist_name;}
+    Point* GetConstVtx(Double_t x, Double_t y, Double_t z);
     Point* GetGausVtx(Double_t x, Double_t sx, Double_t y, Double_t sy, Double_t z, Double_t sz);
     Point* GetUniformVtx(Double_t xmin, Double_t xmax, Double_t ymin, Double_t ymax, Double_t zmin, Double_t zmax);
     Point* GetCustomVtx();
 
   private:
-    const char *input_file_name;
-    const char *input_hist_name;
+    VtxGenerator(VtxGenerator &other);
+    VtxGenerator& operator=(const VtxGenerator& other);
+    
+    const char *xinput_file_name;
+    const char *xinput_hist_name;
+    const char *yinput_file_name;
+    const char *yinput_hist_name;
+    const char *zinput_file_name;
+    const char *zinput_hist_name;
     TFile *input_file;
-    TH1D *input_hist;
+    TH1D *xinput_hist;
+    TH1D *yinput_hist;
+    TH1D *zinput_hist;
     Bool_t used_hist=0;
 };
 #endif
