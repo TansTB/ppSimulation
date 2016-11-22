0714d6dc112e6f981a5b771b8854f47656b77958
Connor Thorburn <connor.thorburn@edu.unito.it>
2016-11-22 17:07:32 +0100

    Hit class update

--- Hit.cxx
+++ Hit.cxx
@@ -2,14 +2,18 @@
 #include "Hit.h"
 using namespace std;
 
-Hit::Hit(Double_t label, Double_t phi, Double_t z){
+Hit::Hit(Double_t label, Double_t phi, Double_t z, Int_t layer){
   this->label = label;
   this->phi = phi;
   this->z = z;
-  if (this->layer == 1) this->r = 4; //radius in cm
-  if (this->layer == 2) this->r = 7; //radius in cm
+  Hit::SetLayer(layer);
 }
 
 void Hit::SetLabel(Double_t label){this->label = label;}
 void Hit::SetPhi(Double_t phi){this->phi = phi;}
 void Hit::SetZ(Double_t z){this->z = z;}
+void Hit::SetLayer(Double_t layer){
+  this->layer = layer;
+  if (layer == 1) r = 4; //radius in cm
+  if (layer == 2) r = 7; //radius in cm
+}
--- Hit.h
+++ Hit.h
@@ -8,14 +8,16 @@
 class Hit:public TObject{
 public:
   Hit(){}
-  Hit(Double_t label, Double_t phi, Double_t z);
+  Hit(Double_t label, Double_t phi, Double_t z, Int_t layer);
   
   void SetLabel(Double_t label);
   void SetPhi(Double_t phi);
   void SetZ(Double_t z);
+  void SetLayer(Double_t layer);
   Double_t GetLabel(){return label;}
   Double_t GetPhi(){return phi;}
   Double_t GetZ(){return z;}
+  Int_t GetLayer(){return layer;}
   
 private:
   Double_t label;
