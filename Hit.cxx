de397b38e1acb6443d7f86bc5846dbc7d8a9e414
Connor Thorburn <connor.thorburn@edu.unito.it>
2016-11-23 09:44:07 +0100

    class Hit updates

--- Hit.cxx
+++ Hit.cxx
@@ -4,18 +4,13 @@
 using namespace std;
 using namespace TMath;
 
-Hit::Hit(Double_t label, Double_t phi, Double_t z, Int_t layer){
+Hit::Hit(Int_t label, Double_t phi, Double_t z){
   this->label = label;
   this->phi = phi;
   this->z = z;
-  Hit::SetLayer(layer);
 }
 
-void Hit::SetLabel(Double_t label){this->label = label;}
+void Hit::SetLabel(Int_t label){this->label = label;}
 void Hit::SetPhi(Double_t phi){this->phi = phi;}
 void Hit::SetZ(Double_t z){this->z = z;}
-void Hit::SetLayer(Double_t layer){
-  this->layer = layer;
-  if (layer == 1) r = 4; //radius in cm
-  if (layer == 2) r = 7; //radius in cm
-}
+
