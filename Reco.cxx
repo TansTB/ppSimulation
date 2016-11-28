2a38fee7faecd48038c2b99bcce1ebb80fae4e7c
Connor Thorburn <connor.thorburn@edu.unito.it>
2016-11-28 13:56:22 +0100

    Updates

--- Reco.cxx
+++ Reco.cxx
@@ -4,52 +4,70 @@
 using namespace std;
 using namespace TMath;
 
+//Official constructor
 //Reco::Reco(TTree *input_tree, vector<vector<string> > configs){
+//this->input_tree = input_tree;
 
-   //this->input_tree = input_tree;
-
-////////////////////////////////TEST CODE//////////////////////////////////
-//________________________________________________________________________
+//Unofficial constructor used for debugging///////////////////////////////////////////////////////
    Reco::Reco(Double_t delta_phi){ 
+   this->delta_phi=delta_phi;
    //Apertura file di input
-   TFile *input_file = new TFile("test.root");
+   TFile *input_file = new TFile("FirstSimulation.root");
    //Lettura TTree  e branch
-   this->input_tree = (TTree*)input_file->Get("T");
-//________________________________________________________________________
+   this->input_tree = (TTree*)input_file->Get("ppSimulation");
+//end of unofficial part of the constructor///////////////////////////////////////////////////////
+     
 
-   //Layer 1
+   //read input tree
+   //input_tree->SetBranchAddress("Multiplicity", &multiplicity);
    TBranch *b1=input_tree->GetBranch("L1_Hits");
    b1->SetAddress(&ptr_L1_hits);
-   Hit * test1 = new Hit::Hit(1,Pi(),1);
-   Hit * test2= new Hit(1,0.5*Pi(),2);
-   //Layer 2
    TBranch *b2=input_tree->GetBranch("L2_Hits");
    b2->SetAddress(&ptr_L2_hits);
-   cout << VertexFinder(test1,test2);
+   
+   //loop on events
+   for(Int_t i=0;i<input_tree->GetEntries();i++){
+      input_tree->GetEvent(i);
+      //get vertex distribution 
+      vertex_distribution->Fill(GetEventVertex());
+   }
+   vertex_distribution->DrawCopy();
+   
+   //cout << multiplicity;
 }
-
 Reco::~Reco(){ delete input_tree;}
 
-void Reco::TreeLooper(){
-   //loop on branches
-   for(Int_t i=0;i<input_tree->GetEntries();i++){
-     input_tree->GetEvent(i);
-     //loop on L1 TClonesArray
-     for(Int_t j=0;j<ptr_L1_hits->GetEntries();j++){ 
-        L1_candidate = (Hit*)ptr_L1_hits->At(j);
-        //loop on L2 TClonesArray
-        for(Int_t k=0;j<ptr_L2_hits->GetEntries();k++){
-            Hit *hit=(Hit*)ptr_L2_hits->At(k);
-            if ((Abs(hit->GetPhi() - L1_candidate->GetPhi()) <= delta_phi)) L2_candidate = hit;
-        }
-     }
-   }
-} 
+Double_t Reco::GetEventVertex(){
+   //loop on L1 TClonesArray
+   for(Int_t j=0;j<ptr_L1_hits->GetEntries();j++){ 
+   L1_candidate = (Hit*)ptr_L1_hits->At(j);
 
-Double_t Reco::VertexFinder (Hit * L1_candidate, Hit * L2_candidate){
+      //loop on L2 TClonesArray
+      for(Int_t k=0;k<ptr_L2_hits->GetEntries();k++){
+         L2_candidate=(Hit*)ptr_L2_hits->At(k);
+
+         // hit match 
+         if (Abs(L2_candidate->GetPhi() - L1_candidate->GetPhi()) <= delta_phi){
+         //comp[j][k] = GetIntersection(L1_candidate,L2_candidate);
+         vertex_candidates->Fill(GetIntersection(L1_candidate,L2_candidate));
+         }
+
+         // no hit match
+         //comp[j][k]=40;
+      }
+   } 
+   //get histogram maximum bin
+   binmax = vertex_candidates->GetMaximumBin();
+   return vertex_candidates->GetXaxis()->GetBinCenter(binmax);
+   //vertex_candidates->DrawCopy();
+}
+
+Double_t Reco::GetIntersection(Hit * L1_candidate, Hit * L2_candidate){
    return (L1_radius*Cos(L1_candidate->GetPhi())*(L2_candidate->GetZ()-L1_candidate->GetZ()))/(L1_radius*Cos(L1_candidate->GetPhi())-L2_radius*Cos(L2_candidate->GetPhi())) + L1_candidate->GetZ();
    
 }
+
+
 
 
 
--- Reco.h
+++ Reco.h
@@ -6,6 +6,7 @@
 #include "TClonesArray.h"
 #include "TTree.h"
 #include "Hit.h"
+#include "Point.h"
 #include "TH1D.h"
 #include "TMath.h"
 #include "MyParser.h"
@@ -25,23 +26,30 @@ public:
    Reco(Double_t delta_phi);  //this just in test mode
    ~Reco();
 
-  void TreeLooper(); //loops on given tree branches and given TClonesArray
-  Double_t VertexFinder(Hit * L1_candidate, Hit * L2_candidate);
+  Double_t GetEventVertex(); //for each event, combines L1 and L2 hits according to delta_phi cut and returns the mode of candidate vertices distribution 
+  Double_t GetIntersection(Hit * L1_candidate, Hit * L2_candidate); //For the provided L1,L2 hit pair, get the intersection between the reconstructed track and plane (z,0,0)
+  //Double_t GetVertex();
   //string* RemoveWhitespaces(string& s); //parser related function
 
   private:
   Reco(Reco &other);
   Reco& operator=(const Reco& other);
+  Double_t GetMultiplicity(){return multiplicity;};
 
   //General Parameters
   Hit *hit;
   Hit *L1_candidate;
   Hit *L2_candidate;
   Double_t delta_phi;
+  Int_t multiplicity;
+  Int_t binmax;
   Double_t L1_radius = 4; //just to test
   Double_t L2_radius = 7; //just to test
+  //Double_t comp[20][20];
   
   //Tree and hit arrays
+  TH1D* vertex_candidates = new TH1D("vertex_candidates","vertex_candidates",300,-15,15);
+  TH1D* vertex_distribution = new TH1D("vertex_distribution","vertex_distribution",100,-1.5,1.5);
   TTree *input_tree;
   TClonesArray *ptr_L1_hits = new TClonesArray("Hit",MAX_SIZE);
   TClonesArray &L1_hits = *ptr_L1_hits;
