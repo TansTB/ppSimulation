050999604b0f873c6c8a25e1012d970df2647184
Connor Thorburn <connor.thorburn@edu.unito.it>
2016-11-28 22:50:48 +0100

    Evening updates

--- Reco.cxx
+++ Reco.cxx
@@ -1,40 +1,34 @@
 #include "Reco.h"
-#include "Hit.cxx"
 
 using namespace std;
 using namespace TMath;
 
-//Official constructor
-//Reco::Reco(TTree *input_tree, vector<vector<string> > configs){
-//this->input_tree = input_tree;
-
-//Unofficial constructor used for debugging///////////////////////////////////////////////////////
-   Reco::Reco(Double_t delta_phi){ 
-   this->delta_phi=delta_phi;
-   //Apertura file di input
-   TFile *input_file = new TFile("FirstSimulation.root");
-   //Lettura TTree  e branch
-   this->input_tree = (TTree*)input_file->Get("ppSimulation");
-//end of unofficial part of the constructor///////////////////////////////////////////////////////
-     
+Reco::Reco(TTree *input_tree, vector<vector<string> > configs){
+   //configuration
+   this->L1_radius = stod(configs.at(28).at(1));
+   this->L2_radius = stod(configs.at(32).at(1));
+   this->delta_phi = stod(configs.at(38).at(1));
 
    //read input tree
-   //input_tree->SetBranchAddress("Multiplicity", &multiplicity);
+   this->input_tree = input_tree;
    TBranch *b1=input_tree->GetBranch("L1_Hits");
    b1->SetAddress(&ptr_L1_hits);
    TBranch *b2=input_tree->GetBranch("L2_Hits");
    b2->SetAddress(&ptr_L2_hits);
+   //DeltaPhiSampling();
    
    //loop on events
    for(Int_t i=0;i<input_tree->GetEntries();i++){
       input_tree->GetEvent(i);
-      //get vertex distribution 
-      vertex_distribution->Fill(GetEventVertex());
+
+      //get average vertex and vertex distribution
+      this->vertex_z += GetEventVertex();  
+      //vertex_distribution->Fill(GetEventVertex());
    }
-   vertex_distribution->DrawCopy();
-   
-   //cout << multiplicity;
+   this->vertex_z /= input_tree->GetEntries(); 
+   //vertex_distribution->DrawCopy(); 
 }
+
 Reco::~Reco(){ delete input_tree;}
 
 Double_t Reco::GetEventVertex(){
@@ -47,19 +41,14 @@ Double_t Reco::GetEventVertex(){
          L2_candidate=(Hit*)ptr_L2_hits->At(k);
 
          // hit match 
-         if (Abs(L2_candidate->GetPhi() - L1_candidate->GetPhi()) <= delta_phi){
-         //comp[j][k] = GetIntersection(L1_candidate,L2_candidate);
-         vertex_candidates->Fill(GetIntersection(L1_candidate,L2_candidate));
-         }
-
-         // no hit match
-         //comp[j][k]=40;
+         if (Abs(L2_candidate->GetPhi() - L1_candidate->GetPhi()) <= delta_phi) vertex_candidates->Fill(GetIntersection(L1_candidate,L2_candidate));      
       }
-   } 
+   }
+ 
    //get histogram maximum bin
    binmax = vertex_candidates->GetMaximumBin();
-   return vertex_candidates->GetXaxis()->GetBinCenter(binmax);
-   //vertex_candidates->DrawCopy();
+   cout << binmax << endl;
+   return vertex_candidates->GetXaxis()->GetBinCenter(binmax); 
 }
 
 Double_t Reco::GetIntersection(Hit * L1_candidate, Hit * L2_candidate){
@@ -67,8 +56,29 @@ Double_t Reco::GetIntersection(Hit * L1_candidate, Hit * L2_candidate){
    
 }
 
+void Reco::DeltaPhiSampling(){
+   //loop on events
+   for(Int_t i=0;i<input_tree->GetEntries();i++){
+      input_tree->GetEvent(i);   
 
+      //loop on L1 TClonesArray
+      for(Int_t j=0;j<ptr_L1_hits->GetEntries();j++){ 
+      L1_candidate = (Hit*)ptr_L1_hits->At(j);
 
+         //loop on L2 TClonesArray
+         for(Int_t k=0;k<ptr_L2_hits->GetEntries();k++){
+         L2_candidate=(Hit*)ptr_L2_hits->At(k);
 
+         // hit match 
+         if (L2_candidate->GetLabel() == L1_candidate->GetLabel()) delta_phi_distribution->Fill(L2_candidate->GetPhi() - L1_candidate->GetPhi()); 
+         }   
+      }
+    }
+   delta_phi_distribution->DrawCopy();
+}
 
+string Reco::RemoveWhitespaces(string& s){
+  s.erase(std::remove_if(s.begin(),s.end(),[](char c){return std::isspace(static_cast<unsigned char>(c));}),s.end());
+  return s;
+}
 
--- Reco.h
+++ Reco.h
@@ -22,38 +22,37 @@ const Int_t MAX_SIZE = 400;
 class Reco{
 public:
    Reco(){}
-   //Reco(TTree *input_tree, vector<vector<string> > configs); 
-   Reco(Double_t delta_phi);  //this just in test mode
+   Reco(TTree *input_tree, vector<vector<string> > configs); 
    ~Reco();
 
   Double_t GetEventVertex(); //for each event, combines L1 and L2 hits according to delta_phi cut and returns the mode of candidate vertices distribution 
   Double_t GetIntersection(Hit * L1_candidate, Hit * L2_candidate); //For the provided L1,L2 hit pair, get the intersection between the reconstructed track and plane (z,0,0)
-  //Double_t GetVertex();
-  //string* RemoveWhitespaces(string& s); //parser related function
+  void DeltaPhiSampling();
+  string RemoveWhitespaces(string& s);
 
   private:
   Reco(Reco &other);
   Reco& operator=(const Reco& other);
-  Double_t GetMultiplicity(){return multiplicity;};
 
   //General Parameters
-  Hit *hit;
   Hit *L1_candidate;
   Hit *L2_candidate;
   Double_t delta_phi;
-  Int_t multiplicity;
+  Double_t vertex_z = 0;
   Int_t binmax;
-  Double_t L1_radius = 4; //just to test
-  Double_t L2_radius = 7; //just to test
-  //Double_t comp[20][20];
+  Double_t L1_radius;
+  Double_t L2_radius; 
   
   //Tree and hit arrays
-  TH1D* vertex_candidates = new TH1D("vertex_candidates","vertex_candidates",300,-15,15);
-  TH1D* vertex_distribution = new TH1D("vertex_distribution","vertex_distribution",100,-1.5,1.5);
+  TH1D* delta_phi_distribution = new TH1D("delta_phi","delta_phi",100,-0.0005,0.0005);
+  TH1D* vertex_candidates = new TH1D("vertex_candidates","vertex_candidates",51,-15,15); 
+  TH1D* vertex_distribution = new TH1D("vertex_distribution","vertex_distribution",51,-15,15); //101,-1.5,1.5
   TTree *input_tree;
   TClonesArray *ptr_L1_hits = new TClonesArray("Hit",MAX_SIZE);
   TClonesArray &L1_hits = *ptr_L1_hits;
   TClonesArray *ptr_L2_hits = new TClonesArray("Hit",MAX_SIZE);
   TClonesArray &L2_hits = *ptr_L2_hits;
+  
+  ClassDef(Reco,1);
 };
 #endif
